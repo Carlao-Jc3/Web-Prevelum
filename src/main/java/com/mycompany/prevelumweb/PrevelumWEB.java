a28b6ba68d64ad5590f9b08e748d3156104ac825
Carslo Eduardo <eduardocarlos.hg@gmail.com>
2025-11-21 18:36:50 -0300

    Pacotes implementados

---
+++ src/main/java/com/mycompany/prevelumweb/PrevelumWEB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prevelumweb;
+
+/**
+ *
+ * @author Carlos Eduardo
+ */
+public class PrevelumWEB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
