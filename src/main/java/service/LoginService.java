d23ec2df2febbb4f120eb372abd6f0dfa5dd9bee
Carslo Eduardo <eduardocarlos.hg@gmail.com>
2025-12-26 15:36:27 -0300

    Update -> LoginService

--- src/main/java/service/LoginService.java
+++ src/main/java/service/LoginService.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package service;
 
-/**
- *
- * @author Carlos Eduardo
- */
+import models.Usuario;
+
 public class LoginService {
-    
+
+    public boolean autenticar(Usuario usuario, String senha) {
+        if (usuario == null) return false;
+        return usuario.getSenha().equals(senha);
+    }
 }
